331e2cfba18e32b49bb802611ea81cd0ec43b468
Pasquale <p.cannavacciuolo@icloud.com>
2022-04-15 15:42:41 +0200

    Initial configuration

---
+++ src/main/java/com/corso/java/corsojava/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.corso.java.corsojava.controller;
+
+import com.corso.java.corsojava.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins="http://localhost:4200")
+@RequestMapping(path = "/api/v1")
+
+public class UserController {
+    @Autowired
+    UserRepository userRepository;
+}
---
+++ src/main/java/com/corso/java/corsojava/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.corso.java.corsojava.repository;
+
+import com.corso.java.corsojava.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
