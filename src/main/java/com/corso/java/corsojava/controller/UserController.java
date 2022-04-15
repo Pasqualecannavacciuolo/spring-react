cea62e54839ef5bad65a08f2c7fd1fcdcc80f0a8
Pasquale <p.cannavacciuolo@icloud.com>
2022-04-15 15:50:46 +0200

    Added implementation

--- src/main/java/com/corso/java/corsojava/controller/UserController.java
+++ src/main/java/com/corso/java/corsojava/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.corso.java.corsojava.controller;
 
+import com.corso.java.corsojava.domain.User;
 import com.corso.java.corsojava.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins="http://localhost:4200")
@@ -13,4 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     UserRepository userRepository;
+
+    @GetMapping(path="/{id}")
+    ResponseEntity<User> findById(@PathVariable String id) {
+        Optional<User> user = userRepository.findById(id);
+        return new ResponseEntity<User>((User) userRepository.findById(id).get(), HttpStatus.OK);
+    }
+
+    @GetMapping(path="/")
+    ResponseEntity<?> findAll() {
+        return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+    }
 }
